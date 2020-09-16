2ab7e43099e3cf587c96823b556f481346714fbc
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-09-16 16:50:54 +0100

    done

--- calculate.php
+++ calculate.php
@@ -1,10 +1,17 @@
 <?php
-
+// Getting and assigning user's input to a variable
 $price = $_POST['price'];
 $tip = $_POST['tipPercentage'];
 
+// Calculting the tip amount
 $tipAmount = $price * ($tip / 100);
 
-echo "The tip amount is ". $tipAmount;
+// Calculating the total amount
+$totalAmount = $tip + $tipAmount;
+
+// Displaying results 
+echo "The tip amount is: ". $tipAmount;
+echo "<br><br>";
+echo "Total amount is: ".$totalAmount;
 
 ?>
